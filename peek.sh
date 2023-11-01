7c9dd42f3a63d953f620c53381855fe9bc928b99
suarez__alvarez <martin.suarezalvarez1@gmail.com>
2023-11-01 19:01:04 +0100

    This is the 3rd version of peek.sh, from 2023.11.1

--- peek.sh
+++ peek.sh
@@ -1,3 +1,24 @@
-head -n $2 $1 
-echo "..."
-tail -n $2 $1
+#! /usr/bin/env bash
+
+# Checking if there was input for the number of lines
+
+if [[ -z $2 ]]
+then
+	lines=3	
+else 
+	lines=$2
+fi
+
+
+
+if [[ $(cat $1 | wc -l) -le $((2*$lines)) ]]
+then
+	cat $1		# if number of lines of file <= 2*input number of lines, then print the whole input file
+
+elif [[ $(cat $1 | wc -l) -gt $((2*$lines)) ]]
+then
+	echo WARNING: Only the first and last $lines lines are printed
+	head -n $lines $1
+	echo "..."
+	tail -n $lines $1   # if number of lines of file > 2*input number of lines, then print $lines first and last lines
+fi		
