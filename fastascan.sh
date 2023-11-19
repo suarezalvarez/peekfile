d780650e3b3d84cd100102eea6faab5e5809e6ec
suarez__alvarez <martin.suarezalvarez1@gmail.com>
2023-11-19 17:47:56 +0100

    fastascan.sh v4, 2023.11.19

--- fastascan.sh
+++ fastascan.sh
@@ -21,15 +21,19 @@ fi
 
 
 ### Report of all the files ###
+
 	# Warning 
+
 echo	
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!WARNING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo
-echo "This script considers a .fasta or .fa file to be a nucleotide (RNA or DNA) sequence if the content of A, C, T, G, U and N in the sequences contained in the file is greater or equal than 90%. If this condition is not fulfilled, the file will automatically be considered a protein sequence file." 
-
+echo "This program considers a .fasta or .fa file to be a nucleotide (RNA or DNA) sequence if the content of A, C, T, G, U and N in the sequences contained in the file is greater or equal than 90%. If this condition is not fulfilled, the file will automatically be considered a protein sequence file." 
 echo 
+echo 'This program considers the first word (delimited by spaces) after the ">" symbol in a fasta header as the identifier of the sequence.'
+echo
+
 	# Header 
 
 echo '================================================================'
@@ -41,7 +45,9 @@ echo
 	
 echo Total number of .fasta and .fa files in the specified folders and its subfolders: $(find $directory -name "*.fasta" -or -name "*.fa" | wc -l) 
 
-echo Number of unique fasta IDs: $(grep ">" $(find $directory -name "*.fasta" -or -name "*.fa") | awk '{print substr($1 , index($1 , ">"))}' | sort | uniq | wc -l) # grep ">" vs cat | awk?
+if [[ $(find $directory -name "*.fasta" -or -name "*.fa" | wc -l) -eq 0 ]]; then exit 1; fi      # exit program if there are no fasta files in the directory
+
+echo Number of unique fasta IDs: $(grep ">" $(find $directory -name "*.fasta" -or -name "*.fa") | awk '{print substr($1 , index($1 , ">"))}' | sort | uniq | wc -l) 
 
 
 
@@ -56,50 +62,97 @@ echo '================== INDIVIDUAL REPORT ==========================='
 echo '================================================================'
 echo 
 
+
+find $directory -name "*.fasta" -or -name "*.fa" | while read file; do 			# loop over each file name
+
+	if ! grep -q ">" "$file"; then 							# go to next file if there are no fasta headers in the current file (to avoid reading binary files)
+	echo ========== "$file" ==========
+	echo
+	echo This is file does not contain sequences in fasta format
+	continue
+	 
+	fi 
+
+	# number of characters in sequences of the file that may be nucleotides -- considering mainly A/a, C/c, T/t, G/g, U/u and N/n as the characters that encode nucleotides that will be mostly present in the sequences
+		
+	num_nucleotides=$(grep -v ">" "$file" | tr -d "\n""\-"" " | awk -F '' '{for (i=1; i<=length($0); i++) { print $i }}' | grep -ic [ACTGUN])
+	
+
+	# proportion of these characters withrespect to the rest of characters of the sequences of the file
+	
+	total_chars_sequences=$(grep -v ">" "$file" | tr -d "\n"" ""\-" | wc -m)
+	
+	nucleotide_pct=$(( 100*(num_nucleotides)/$total_chars_sequences ))
+	
+	
+	
+	
 	# Report
 	
-find $directory -name "*.fasta" -or -name "*.fa" | while read file; do
-
-	echo ===== $file ===== 				# header with file name
+	
+	
+	if [[ $nucleotide_pct -ge 90 ]]; then 						# If nucleotide_pct >= 90%, the fasta is considered a nucleic acid fasta
+	
+	
+		echo
+		echo ========== "$file" [NUCLEIC ACID FASTA FILE] ========== 			# header with file name for nucleic acids
+		echo
+	
+	
+		echo This file contains $(grep -c ">" "$file") nucleic acid 'sequence(s)' 		# number of fasta headers ">"
+		echo
+		echo The total number of nucleotides in the sequences of this file is: $total_chars_sequences
+	
+	
+	
+	
+	else
+		echo
+		echo ========== "$file" [PROTEIN FASTA FILE] ==========  		 	# header with file name for proteins
+		echo	
+		
+		
+		echo This file contains $(grep -c ">" "$file") protein 'sequence(s)' 		# number of fasta headers ">"
+		echo
+		echo The total number of amino acids in the sequences of this file is: $total_chars_sequences
+	fi
 	echo
-	if [[ -h $file ]]; then	 			# is it a symlink?
+	
+	
+	
+	
+	if [[ -h "$file" ]]; then	 							# is it a symlink?
 		echo This file is a symbolic link
 	else
 		echo This file is not a symbolic link
 	fi
-	
 	echo
 	
-	# number of characters in sequences of the file that may be nucleotides -- considering mainly A, C, T, G, U and N as the characters that encode nucleotides that will be mostly present in the sequences
 	
-	num_A=$(grep -v ">" $file | tr -d "\n""\-"" " | awk -F '' '{for (i=1; i<=length($0); i++) { print $i }}' | grep -c A)
 	
-	num_C=$(grep -v ">" $file | tr -d "\n""\-"" " | awk -F '' '{for (i=1; i<=length($0); i++) { print $i }}' | grep -c C)
 	
-	num_T=$(grep -v ">" $file | tr -d "\n""\-"" " | awk -F '' '{for (i=1; i<=length($0); i++) { print $i }}' | grep -c T)
 	
-	num_G=$(grep -v ">" $file | tr -d "\n""\-"" " | awk -F '' '{for (i=1; i<=length($0); i++) { print $i }}' | grep -c G)
 	
-	num_N=$(grep -v ">" $file | tr -d "\n""\-"" " | awk -F '' '{for (i=1; i<=length($0); i++) { print $i }}' | grep -c N)
+	if [[ $lines -eq 0 ]]; then								# if $lines == 0 go to the next file
+		continue
+		
+	elif [[ $(cat "$file" | wc -l) -le $((2*$lines)) ]]; then
 	
-	num_U=$(grep -v ">" $file | tr -d "\n""\-"" " | awk -F '' '{for (i=1; i<=length($0); i++) { print $i }}' | grep -c U)
+	echo '-- previsualization of the file content--'
+	echo 
+	cat "$file"										# if number of lines of file <= 2*input number of lines, then print the whole input file
 
-	# proportion of these characters withrespect to the rest of characters of the sequences of the file
-	
-	total_chars_sequences=$(grep -v ">" $file | tr -d "\n"" ""\-" | wc -m)
-	
-	nucleotide_pct=$(( 100*(num_A+num_C+num_T+num_G+num_N+num_U)/$total_chars_sequences  ))
-	
-	# If nucleotide_pct >= 90%, the fasta is considered a nucleic acid fasta
-	if [[ $nucleotide_pct -ge 90 ]]; then
-		echo This file contains $(grep -c ">" $file) nucleic acid sequences # number of fasta headers ">"
-		echo The total number of nucleotides in the sequences of this file is: $total_chars_sequences
-	
+
 	else
-		echo This file contains $(grep -c ">" $file) protein sequences # number of fasta headers ">"
-		echo The total number of amino acids in the sequences of this file is: $total_chars_sequences
-	fi
+	echo '-- previsualization of the file content --'
 	echo
+	echo "[!!!]WARNING: Only the first and last $lines lines are printed[!!!]"
+	echo
+	head -n $lines "$file"
+	echo "..."
+	tail -n $lines "$file"   								# if number of lines of file > 2*$lines, then print "$lines" first and last lines
+fi		
+	
 done
 
 
