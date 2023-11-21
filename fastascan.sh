b6bc08985e229523ca86022ac9c8fae1b69a0ad8
suarez__alvarez <martin.suarezalvarez1@gmail.com>
2023-11-21 11:08:42 +0100

    fastascan.sh v6, 2023.11.21

--- fastascan.sh
+++ fastascan.sh
@@ -1,23 +1,41 @@
 #!/usr/bin/env bash
 
-### Defining directory ###
-
-if [[ -z $1 ]]
-then
-	directory=$PWD
-else
-	directory=$1
-fi
-
 
 ### Defining number of lines ###
 
 if [[ -z $2 ]]
 then
 	lines=0 
+elif echo $2 | grep -q "\."; then
+	lines=$(echo $2 | awk '{print substr($0 , 1 , index($0 , ".")-1)}') # if $2 is a floating point number, remove point 
+
+elif ! echo $2 | grep -qE '^[0-9]+$' && [[ -n $2 ]]; then
+	echo
+	echo Please, introduce a valid number of lines
+	echo
+	exit 3
+
 else
 	lines=$2
 fi
+
+
+### Defining directory ###
+
+if [[ -z $1 ]]
+then
+	directory=$PWD
+	
+elif echo $1 | grep -qE '^[0-9]+$' && [[ -z $2 ]]; then 	# if the 1st argument is an integer, and there is not 2nd argument, interpret the 1st as the number of lines
+	lines=$1
+	directory=$PWD
+
+else
+	directory=$1
+fi
+
+
+
 
 
 ### Report of all the files ###
@@ -26,9 +44,10 @@ fi
 
 echo	
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-echo "!!!!!!!!!!!!!!!!!!!!!!!!!!WARNING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "!!!!!!!!!!!!!!!!!!!IMPORTANT INFORMATION!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-echo "This program uses 2 arguments: the directory and the number of lines of each file to show. If you only specify one of them, it will be interpreted as the directory by default."
+echo "This program uses 2 arguments: the directory and the number of lines of each file to show." 
+echo "If you only specify one of them, it will be interpreted as the number of lines if this argument is an integer, and as the directory if it is a text string."
 echo
 echo "This program considers a .fasta or .fa file to be a nucleotide (RNA or DNA) sequence if the content of A, C, T, G, U and N (upper or lower case) in the sequences contained in the file is greater or equal than 90%. If this condition is not fulfilled, the file will automatically be considered a protein sequence file." 
 echo 
@@ -43,12 +62,16 @@ echo '================================================================'
 echo
 
 	# Number of files and IDs
-	
-echo Total number of .fasta and .fa files in the specified folders and its subfolders: $(find $directory -name "*.fasta" -or -name "*.fa" -type l -or -type f| wc -l) 
 
-if [[ $(find $directory -name "*.fasta" -or -name "*.fa" | wc -l) -eq 0 ]]; then exit 1; fi      # exit program if there are no fasta files in the directory
+if [[ ! -d $directory ]]; then echo The directory $(echo \'$directory\') does not exist; echo ; exit 2; fi # if the directory doesn't exist, exit
 
-echo Number of unique fasta IDs: $(grep ">" $(find $directory -name "*.fasta" -or -name "*.fa") | awk '{print substr($1 , index($1 , ">"))}' | sort | uniq | wc -l) 
+
+echo Total number of .fasta and .fa files in the specified folders and its subfolders: $(find $directory \( -name "*.fasta" -or -name "*.fa" \) \( -type f -or -type l \) | wc -l) 
+
+if [[ $(find $directory \( -name "*.fasta" -or -name "*.fa" \) \( -type f -or -type l \) | wc -l) -eq 0 ]]; then exit 1; fi      # exit program if there are no fasta files in the directory
+
+
+echo Number of unique fasta IDs: $(grep ">" $(find $directory \( -name "*.fasta" -or -name "*.fa" \) \( -type f -or -type l \) ) | awk '{print substr($1 , index($1 , ">"))}' | sort | uniq | wc -l) 
 
 
 
@@ -64,12 +87,9 @@ echo '================================================================'
 echo 
 
 
-find $directory -name "*.fasta" -or -name "*.fa" | while read file; do 			# loop over each file name
-	
-	if [[ -d "$file" ]]; then # if there's a directory with a name that ends in .fasta, skip it
-	continue
+find $directory \( -name "*.fasta" -or -name "*.fa" \) \( -type f -or -type l \) | while read file; do 			# loop over each file name
 
-	elif ! awk -F '' 'NR==1{print $1}' "$file" | grep -q ">" ; then 							# go to next file if the first line is not a fasta header (to avoid reading binary files)
+	if ! awk -F '' 'NR==1{print $1}' "$file" | grep -q ">" ; then 							# go to next file if the first line is not a fasta header (to avoid reading binary files)
 	echo ========== "$file" ==========
 	echo
 	echo This file does not contain sequences in fasta format
@@ -154,8 +174,7 @@ find $directory -name "*.fasta" -or -name "*.fa" | while read file; do 			# loop
 	head -n $lines "$file"
 	echo "..."
 	tail -n $lines "$file"   								# if number of lines of file > 2*$lines, then print "$lines" first and last lines
+
 fi		
 	
 done
-
-
